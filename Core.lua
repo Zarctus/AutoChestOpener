5080c937f32303d18a5b7a8c0d65e62374f2ba74
Zarctus <zarctus@gmail.com>
2026-04-27 21:27:10 +0200

    Mise à jour de la version à 2.2.2 dans le fichier .toc et le module principal, ajustement du changelog pour refléter la nouvelle version.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 --[[
     Auto Chest Opener - Core Module
     Automatically opens all types of containers, chests, bags, crates, lockboxes, gifts and more
-    Version: 2.2.1
+    Version: 2.2.2
 ]]
 
 local addonName, ACO = ...
